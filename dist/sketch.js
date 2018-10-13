37d2bdb027523fb047ed353d0382a4942574b7c5
TiPiKej <romukkemot@gmail.com>
2018-10-14 00:24:06 +0200

    1.0
    created basic fractal tree with L System

---
+++ dist/sketch.js
@@ -0,0 +1,71 @@
+const angleCount = 30;
+const cuttingLength = 0.56;
+const axiom = "F";
+
+let len = 100;
+let genStr = axiom;
+let angle;
+
+let rules = [];
+rules[0] = {
+  a: "F",
+  b: "FF+[+F-F-F]-[-F+F+F]"
+};
+
+function generate() {
+  let newString = "";
+
+  Array.from(genStr).forEach(current => {
+    Array.from(rules).some(el => {
+      if (el.a === current) {
+        newString += el.b;
+        return el.a === current;
+      } else newString += current;
+    });
+  });
+
+  genStr = newString;
+
+  len *= cuttingLength;
+  drawLine();
+}
+
+function drawLine() {
+  background(51);
+  stroke(255, 100);
+
+  resetMatrix();
+  translate(width / 2, height);
+
+  Array.from(genStr).forEach(current => {
+    switch (current) {
+      case "F":
+        line(0, 0, 0, -len);
+        translate(0, -len);
+        break;
+      case "+":
+        rotate(angle);
+        break;
+      case "-":
+        rotate(-angle);
+        break;
+      case "[":
+        push();
+        break;
+      case "]":
+        pop();
+        break;
+    }
+  });
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+  angle = radians(angleCount);
+  drawLine();
+
+  createP();
+  const button = createButton("Click to generate");
+  button.mousePressed(generate);
+}
